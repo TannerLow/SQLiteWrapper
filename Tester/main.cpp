e98fa3f9dc5b3bfc2d41bc9e56c88d9a66c5c302
Tanner Lowthorp <lowthorpt@gmail.com>
2025-11-16 13:35:50 -0600

    deleted db file from remote

--- Tester/main.cpp
+++ Tester/main.cpp
@@ -12,19 +12,95 @@ int main() {
 
 	sql::Database db = std::move(opened.db);
 
-	sql::Statement::CreateResult created = db.prepare("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, name TEXT);");
-	if (not created.successful) {
-		std::cout << created.error << std::endl;
+	sql::Statement::CreateResult createdCreate = db.prepare("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, name TEXT);");
+	if (not createdCreate.successful) {
+		std::cout << createdCreate.error << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	sql::Statement::StepResult stepped = created.statement.step();
+	sql::Statement::StepResult createStep;
 	do {
-		if (not stepped.successful) {
-			std::cout << stepped.error << std::endl;
+		createStep = createdCreate.statement.step();
+
+		if (not createStep.successful) {
+			std::cout << createStep.error << std::endl;
 			return EXIT_FAILURE;
 		}
-	} while (not stepped.done);
+	} while (not createStep.done);
+
+	sql::Statement::CreateResult createdInsert = db.prepare("INSERT OR IGNORE INTO test(id, name) VALUES (?, ?);");
+	if (not createdInsert.successful) {
+		std::cout << createdInsert.error << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::optional<std::string> err = createdInsert.statement.bind(1, 1);
+	if (err.has_value()) {
+		std::cout << err.value() << std::endl;
+	}
+	err = createdInsert.statement.bind(2, "Tanner");
+	if (err.has_value()) {
+		std::cout << err.value() << std::endl;
+	}
+
+	sql::Statement::StepResult insertStep;
+	do {
+		insertStep = createdInsert.statement.step();
+
+		if (not insertStep.successful) {
+			std::cout << insertStep.error << std::endl;
+			return EXIT_FAILURE;
+		}
+	} while (not insertStep.done);
+
+	createdInsert = db.prepare("INSERT OR IGNORE INTO test(id, name) VALUES (?, ?);");
+	if (not createdInsert.successful) {
+		std::cout << createdInsert.error << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	err = createdInsert.statement.bind(1, 2);
+	if (err.has_value()) {
+		std::cout << err.value() << std::endl;
+	}
+	err = createdInsert.statement.bind(2, "Tony");
+	if (err.has_value()) {
+		std::cout << err.value() << std::endl;
+	}
+
+	do {
+		insertStep = createdInsert.statement.step();
+
+		if (not insertStep.successful) {
+			std::cout << insertStep.error << std::endl;
+			return EXIT_FAILURE;
+		}
+	} while (not insertStep.done);
+
+	sql::Statement::CreateResult createdSelect = db.prepare("SELECT * FROM test;");
+	if (not createdSelect.successful) {
+		std::cout << createdSelect.error << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	sql::Statement::StepResult selectStep;
+	do {
+		selectStep = createdSelect.statement.step();
+
+		if (not selectStep.successful) {
+			std::cout << selectStep.error << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		if (not selectStep.done) {
+			std::cout << createdSelect.statement.column_int(0) << ' ';
+			std::optional<std::string> name = createdSelect.statement.column_text(1);
+			if (name.has_value()) {
+				std::cout << name.value();
+			}
+			std::cout << std::endl;
+		}
+	} while (not selectStep.done);
 
 	return 0;
 }
